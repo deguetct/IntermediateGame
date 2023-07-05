789b0b608cdd707cd6dd82931af8375c447d9c56
AIE\s213165 <deguetct@gmail.com>
2023-07-05 18:47:10 +0930

    no message

--- Intermediate Game/Assets/Scripts/GameMenu.cs
+++ Intermediate Game/Assets/Scripts/GameMenu.cs
@@ -10,12 +10,52 @@ using UnityEngine.Events;
 public class GameMenu : MonoBehaviour
 {
 
-    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject titleOne;
+    [SerializeField] private GameObject homeButton;
+    [SerializeField] private GameObject newTitle;
+    [SerializeField] private GameObject backButton;
+    [SerializeField] private GameObject settingsButton;
+
+    private bool settingsOpen = false;
+
+    void Update()
+    {
+        if(settingsOpen == true)
+        {
+            titleOne.SetActive(false);
+            homeButton.SetActive(false);
+            settingsButton.SetActive(false);
+
+            newTitle.SetActive(true);
+            backButton.SetActive(true);
+
+        }else if(settingsOpen == false)
+        {
+            titleOne.SetActive(true);
+            homeButton.SetActive(true);
+            settingsButton.SetActive(true);
+
+            newTitle.SetActive(false);
+            backButton.SetActive(false);
+        }
+    }
 
     public void LoadPreviousScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
+    public void SettingsOpen()
+    {
+        if(settingsOpen == false)
+        {
+            settingsOpen = true;
+
+        }else if(settingsOpen == true)
+        {
+            settingsOpen = false;
+        }
+
+    }
 
 }
